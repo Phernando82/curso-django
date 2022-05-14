c8d6c76dee0d3b2493d5aed900ce92309ab512b2
Phernando <nandovalverde@gmail.com>
2022-05-14 17:50:30 +0100

    Adicionada Django Debug Toolbar

--- pypro/settings.py
+++ pypro/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
-from decouple import config
+from decouple import config, Csv
 from django.contrib import staticfiles
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,6 +75,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pypro.wsgi.application'
+
+# Configuração Django Debug Toolbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
