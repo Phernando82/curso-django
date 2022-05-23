fc868bf40fcb0baaae13753f49ed49f0352ebeab
Phernando <nandovalverde@gmail.com>
2022-05-23 21:08:07 +0100

    Criação de modelos nos testes.

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -2,13 +2,12 @@ import pytest
 from django.urls import reverse
 from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
+from from model_bakery import baker
 
 
 @pytest.fixture
 def video(db):
-    v = Video(slug='u2', titulo='Video Aperitivo: U2', vimeo_id='710132185')
-    v.save()
-    return v
+    return baker.m
 
 
 @pytest.fixture
