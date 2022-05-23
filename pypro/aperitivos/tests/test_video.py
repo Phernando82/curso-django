75470d9e547146c1ffd9fc6bb7508784a394bd5c
Phernando <nandovalverde@gmail.com>
2022-05-23 21:35:05 +0100

    Criação automática de propriedades de modelos

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -1,13 +1,14 @@
 import pytest
 from django.urls import reverse
-from model_bakery import baker
+from model_mommy import mommy
 
+from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
 def video(db):
-    return baker.make(video)
+    return mommy.make(Video)
 
 
 @pytest.fixture
