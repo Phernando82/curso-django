0f2a2604d51342f075c8dd4439dc0fd08621340e
Phernando <nandovalverde@gmail.com>
2022-05-24 17:29:35 +0100

    Migração de dados

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -8,4 +8,4 @@ def listar_modulos_ordenados()-> List[Modulo]:
     Lista Módulos ordenados por títulos
     return None
     """
-    return list(Modulo.objects.order_by('titulo').all())
+    return list(Modulo.objects.order_by('order').all())
